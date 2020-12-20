07aef17c9cf99ae561745f7350a5967f077dcc3b
Pinjuf <61752900+Pinjuf@users.noreply.github.com>
2020-12-20 18:54:24 +0100

    Add files via upload

--- LM317_calc.py
+++ LM317_calc.py
@@ -1,5 +1,7 @@
 import math
 
+power_range = 1
+
 Vout = float(input("[?] Vout: "))
 
 standard_resistor_values_base = [1, 1.1, 1.2, 1.3, 1.5, 1.6, 1.8, 2, 2.2, 2.4, 2.7, 3, 3.3, 3.6, 3.9, 4.3, 4.7, 5.1, 5.6, 6.2, 6.8, 7.5, 8.2, 9.1]
@@ -9,10 +11,13 @@ def get_ratio(v):
     v -= 1
     return v
 
-multipliers = [10**math.floor(math.log10(get_ratio(Vout))-1), 10**math.floor(math.log10(get_ratio(Vout))), 10**math.floor(math.log10(get_ratio(Vout))+1)]
+
 lowest_difference = [0, 0, math.inf]
 for R1 in standard_resistor_values_base:
     R2_opt = R1*get_ratio(Vout)
+    basic_range = math.floor(math.log10(R1*R2_opt))
+    extended_range = range(basic_range-power_range, basic_range+power_range+1)
+    multipliers = [10**i for i in extended_range]
     for m in multipliers:
         for R2 in standard_resistor_values_base:
             R2 *= m
@@ -24,6 +29,6 @@ for R1 in standard_resistor_values_base:
 print()
 print("Ratio R1/R2: ", get_ratio(Vout))
 print()
-print("Optimized resistor values:")
+print("Optimized resistor values (multiple can also be used, of course):")
 print("R1: ", lowest_difference[0])
 print("R2: ", lowest_difference[1])
